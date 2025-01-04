a7e97d7bf61e91831106e5468b4b9190d62d104f
Jason Eximoelle <eximoelle@proton.me>
2025-01-04 18:51:30 +1000

    Remove some unused stuff

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config

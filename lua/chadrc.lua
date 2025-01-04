2b2abb6faa74d46db8983e6f22ee7cbf84d44e9c
Jason Eximoelle <eximoelle@proton.me>
2025-01-04 18:43:15 +1000

    Statusline theme changed

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,12 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "kanagawa",
+  theme = "kanagawa",
   transparency = true,
 
   hl_override = {
@@ -29,8 +29,8 @@ M.base46 = {
 
 M.ui = {
   statusline = {
-    theme = "minimal",
-  }
+    theme = "vscode_colored",
+  },
 }
 
 return M
